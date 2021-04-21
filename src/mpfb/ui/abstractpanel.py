59a6fbee8db7db1a24c685c29960b23e84612af9
Joel Palmius <joepal1976@hotmail.com>
2021-04-21 07:56:11 +0200

    Abstract class for panels

---
+++ src/mpfb/ui/abstractpanel.py
@@ -0,0 +1,22 @@
+"""File containing the base class for UI panels"""
+
+import bpy
+from mpfb.services.logservice import LogService
+from mpfb.services.uiservice import UiService
+
+_LOG = LogService.get_logger("ui.abstractpanel")
+
+class Abstract_Panel(bpy.types.Panel):
+    """Human modeling panel."""
+
+    bl_label = "Abstract panel"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = UiService.get_value("DEVELOPERCATEGORY")
+    bl_options = {'DEFAULT_CLOSED'}
+
+    def create_box(self, layout, box_text, icon=None):
+        _LOG.enter()
+        box = layout.box()
+        box.label(text=box_text)
+        return box
