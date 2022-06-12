33a819076e7cbdc736c3e2893e127ec67cc1d6c8
Joel Palmius <joepal1976@hotmail.com>
2022-06-12 11:14:08 +0200

    Tidy up sculpt operation

--- src/mpfb/ui/sculpt/operators/setupsculpt.py
+++ src/mpfb/ui/sculpt/operators/setupsculpt.py
@@ -11,12 +11,11 @@ from bpy.types import StringProperty
 from bpy_extras.io_utils import ImportHelper
 
 _LOG = LogService.get_logger("sculpt.operators.setupsculpt")
-_LOG.set_level(LogService.DEBUG)
 
 class MPFB_OT_Setup_Sculpt_Operator(bpy.types.Operator):
-    """Bake all shape keys into a final mesh and optionally perform other operations suitable for setting up a sculpt project"""
+    """Bake all shape keys into a final mesh and optionally perform other operations suitable for starting a sculpt project. WARNING: You will no longer be able to adjust targets after doing this."""
     bl_idname = "mpfb.setup_sculpt"
-    bl_label = "Bake mesh for sculpt"
+    bl_label = "Set up mesh for sculpt"
     bl_options = {'REGISTER', 'UNDO'}
 
     @classmethod
@@ -284,119 +283,6 @@ class MPFB_OT_Setup_Sculpt_Operator(bpy.types.Operator):
 
             self._handle_bm(context, obj, delete_helpers)
             self._setup_multires(context, obj, setup_multires, subdivisions, multires_first)
-
-
-#===============================================================================
-#         if sculpt_strategy in ["DESTCOPY", "SOURCEDESTCOPY"]:
-#             bpy.ops.object.duplicate(linked=False)
-#             dest = context.object
-#             dest.name = "Bake destination"
-#             dest.parent = None
-#             _LOG.debug("Dest object", dest)
-#
-#             if objtype == "Basemesh":
-#                 self._handle_bm(context, dest, delete_helpers)
-#
-#             self._handle_armature(context, dest, apply_armature)
-#
-#
-#             if remove_delete:
-#                 for modifier in dest.modifiers:
-#                     if modifier.type == 'MASK' and modifier.vertex_group != 'body':
-#                         dest.modifiers.remove(modifier)
-#
-#             self._clear_subdiv(context, dest)
-#
-#             if sculpt_strategy == "SOURCEDESTCOPY":
-#                 bpy.ops.object.duplicate(linked=False)
-#                 source = context.object
-#                 source.name = "Sculpt source"
-#                 source.parent = None
-#                 _LOG.debug("Source object", source)
-#             else:
-#                 source = obj
-#
-#             if normal_material:
-#                 MaterialService.delete_all_materials(dest)
-#                 material = MaterialService.create_empty_material('Material for baking normal map', dest)
-#                 material.diffuse_color = MaterialService.get_skin_diffuse_color()
-#                 nodes = material.node_tree
-#                 principled = NodeService.find_first_node_by_type_name(nodes, 'ShaderNodeBsdfPrincipled')
-#                 principled.inputs['Base Color'].default_value = MaterialService.get_skin_diffuse_color()
-#                 imgtex = NodeService.create_image_texture_node(nodes, xpos=-700)
-#                 side = int(resolution)
-#                 bpy.ops.image.new(name='Sculpt normal map', width=side, height=side)
-#                 imgtex.image = bpy.data.images['Sculpt normal map']
-#
-#             if setup_multires:
-#                 modifier = dest.modifiers.new('Sculpt subdivisions', 'SUBSURF')
-#                 modifier.levels = 0
-#                 modifier.render_levels = subdivisions
-#
-#             dest.select_set(state=False)
-#             dest.hide_viewport = True
-#
-#         obj.select_set(state=True)
-#         context.view_layer.objects.active = obj
-#
-#         _LOG.debug("Selected object", context.object)
-#
-#         if sculpt_strategy == "ORIGIN" and source:
-#             if objtype == "Basemesh":
-#                 self._handle_bm(context, source, delete_helpers)
-#
-#             self._handle_armature(context, source, apply_armature, source != obj)
-#
-#             if remove_delete:
-#                 for modifier in source.modifiers:
-#                     if modifier.type == 'MASK' and modifier.vertex_group != 'body':
-#                         source.modifiers.remove(modifier)
-#
-#             self._clear_subdiv(context, source)
-#
-#             if obj != source:
-#                 obj.select_set(state=False)
-#             source.select_set(state=True)
-#             context.view_layer.objects.active = source
-#
-#         if sculpt_strategy == "SOURCEDESTCOPY":
-#             if delete_origin:
-#                 parent = obj
-#                 if obj.parent:
-#                     parent = obj.parent
-#
-#                 for child in ObjectService.get_list_of_children(parent):
-#                     bpy.data.objects.remove(child, do_unlink=True)
-#
-#                 bpy.data.objects.remove(parent, do_unlink=True)
-#
-#         _LOG.debug("Source object", source)
-#         _LOG.debug("Setup multires", setup_multires)
-#
-#         if setup_multires:
-#             for modifier in source.modifiers:
-#                 if modifier.type == 'SUBSURF':
-#                     source.modifiers.remove(modifier)
-#             modifier = source.modifiers.new('Sculpt multires', 'MULTIRES')
-#             if multires_first:
-#                 while source.modifiers.find(modifier.name) != 0:
-#                     bpy.ops.object.modifier_move_up({'object': source}, modifier=modifier.name)
-#             if subdivisions > 0:
-#                 for n in range(subdivisions):
-#                     bpy.ops.object.multires_subdivide(modifier="Sculpt multires")
-#             #modifier.levels = 0
-#
-#         if sculpt_strategy != "ORIGIN" and adjust_settings:
-#             scene = context.scene
-#             scene.cycles.samples = 8
-#             scene.cycles.adaptive_min_samples = 1
-#             scene.cycles.use_denoising = False
-#             scene.cycles.bake_type = 'NORMAL'
-#             scene.render.use_bake_multires = False
-#             scene.render.bake.use_selected_to_active = True
-#             scene.render.bake.cage_extrusion = 0.01
-#             scene.render.bake.max_ray_distance = 0.1
-#===============================================================================
 
         if sculpt_strategy != "ORIGIN" and adjust_settings:
             scene = context.scene
