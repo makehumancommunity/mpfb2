0fd530d5256c64b0debda8de6369028f7802c8c7
Joel Palmius <joepal1976@hotmail.com>
2022-10-04 13:59:31 +0200

    Big bang fix of tabs to spaces in json files

--- fixfiles.bash
+++ fixfiles.bash
@@ -2,3 +2,5 @@
 
 # Remove trailing whitespace
 find src/mpfb -name "*.py" -exec "sed" "-i" "-e" 's/[ \t]*$//' {} ";"
+find src/mpfb -name "*.json" -exec "sed" "-i" "-e" 's/[ \t]*$//' {} ";"
+find src/mpfb -name "*.json" -exec "sed" "-i" "-e" 's/[\x09\t]/    /g' {} ";"
