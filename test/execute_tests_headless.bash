e5bd8371781de717e2a2a77a93a79920468780a7
Joel Palmius <joepal1976@hotmail.com>
2022-12-17 14:40:51 +0100

    Delete old coverage info before running new tests

--- test/execute_tests_headless.bash
+++ test/execute_tests_headless.bash
@@ -2,6 +2,9 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+rm -rf coverage
+rm .coverage
+
 if [ -z "$BLENDER_EXE" ]; then
   echo "Must set the BLENDER_EXE environment variable to point at the blender executable"
 else
