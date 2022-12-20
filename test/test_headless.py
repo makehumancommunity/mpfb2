7e056c654ebc88cabf26c99d8f9639da77136b47
Joel Palmius <joepal1976@hotmail.com>
2022-12-20 10:46:16 +0100

    Ability to limit testing to file specified in env TEST_MODULE

--- test/test_headless.py
+++ test/test_headless.py
@@ -1,7 +1,12 @@
-import pytest
+import pytest, os
 from mpfb.services.locationservice import LocationService
 
 tests = LocationService.get_mpfb_test("tests")
+
+if "TEST_MODULE" in os.environ:
+    tests = os.path.abspath(os.path.join(tests, "..", os.environ['TEST_MODULE']))
+    print("TEST_MODULE is set: " + tests)
+
 retcode = pytest.main(["--cov-report", "html:coverage", "--cov", "-x", tests, "-v"])
 if retcode:
     print("Unit tests have finished with error code " + str(retcode) + ". See console output for results.")
