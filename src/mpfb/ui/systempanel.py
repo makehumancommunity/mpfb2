6fa9c51c895a4d6b36aae2f7bc5d7ae4eb5c68a7
Joel Palmius <joepal1976@hotmail.com>
2023-08-25 09:02:32 +0200

    Add a panel for displaying build info

--- src/mpfb/ui/systempanel.py
+++ src/mpfb/ui/systempanel.py
@@ -1,8 +1,10 @@
+import bpy, sys
 from mpfb._classmanager import ClassManager
 from mpfb.services.logservice import LogService
 from mpfb.services.uiservice import UiService
 from mpfb.services.objectservice import ObjectService
 from mpfb.ui.abstractpanel import Abstract_Panel
+from mpfb import BUILD_INFO, VERSION
 
 _LOG = LogService.get_logger("ui.systempanel")
 
@@ -10,9 +12,17 @@ class MPFB_PT_System_Panel(Abstract_Panel):
     bl_label = "System and resources"
     bl_category = UiService.get_value("DEVELOPERCATEGORY")
 
+    def _system_info(self, context, scene, layout):
+        box = self._create_box(layout, "System information", "TOOL_SETTINGS")
+        box.label(text="Build info: %s" % str(BUILD_INFO))
+        box.label(text="Blender Version: %s" % str(bpy.app.version))
+        pyver = [sys.version_info[0], sys.version_info[1], sys.version_info[2]]
+        box.label(text="Python Version: %s" % str(pyver))
+
     def draw(self, context):
         _LOG.enter()
         layout = self.layout
-        scn = context.scene
+        scene = context.scene
+        self._system_info(context, scene, layout)
 
 ClassManager.add_class(MPFB_PT_System_Panel)
