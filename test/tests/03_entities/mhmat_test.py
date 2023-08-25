f23dd0cd8a38ff8a066f04b9ce9b963e97697321
Joel Palmius <joepal1976@hotmail.com>
2023-08-25 08:11:49 +0200

    Remove now useless mhmat test

--- test/tests/03_entities/mhmat_test.py
+++ test/tests/03_entities/mhmat_test.py
@@ -14,7 +14,7 @@ def test_mhmat_texture_keys():
         "displacementmapTexture",
         "specularmapTexture",
         "transmissionmapTexture",
-        "transparencymapTexture",
+        "opacitymapTexture",
         "roughnessmapTexture",
         "metallicmapTexture",
         "aomapTexture",
@@ -31,33 +31,6 @@ def test_mhmat_texture_keys():
 
     for key in texture_keys:
         assert key in known_keys
-        assert key.lower() in MHMAT_NAME_TO_KEY
-
-def test_mhmat_alias_keys():
-    """MHMAT alias keys"""
-    from mpfb.entities.material.mhmatkeys import MHMAT_KEYS, MHMAT_NAME_TO_KEY
-    assert MHMAT_KEYS
-
-    alias_keys = [
-        "diffusemapTexture",
-        "albedoTexture",
-        "albedoMapTexture",
-        "basecolorTexture",
-        "basecolorMapTexture",
-        "opacityTexture",
-        "opacityMapTexture",
-        "emissiveTexture",
-        "emissionTexture",
-        "sssTexture",
-        "sssMapTexture"
-        ]
-
-    known_keys = []
-
-    for key in MHMAT_KEYS:
-        known_keys.append(key.key_name)
-
-    for key in alias_keys:
         assert key.lower() in MHMAT_NAME_TO_KEY
 
 def test_load_mhmat_file():
