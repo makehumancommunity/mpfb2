617ddc408c56fbfd68322d3ee692efde19995a2c
Joel Palmius <joepal1976@hotmail.com>
2023-08-27 16:19:17 +0200

    Provide UI for overriding second root on a per-blend case, for issue #130

--- src/mpfb/ui/assetlibrary/assetsettingspanel.py
+++ src/mpfb/ui/assetlibrary/assetsettingspanel.py
@@ -2,16 +2,20 @@
 
 from mpfb._classmanager import ClassManager
 from mpfb.services.logservice import LogService
+from mpfb.services.locationservice import LocationService
 from mpfb.services.uiservice import UiService
+from mpfb.services.assetservice import AssetService
 from mpfb.services.sceneconfigset import SceneConfigSet
 from mpfb.ui.abstractpanel import Abstract_Panel
-import os
+import os, bpy
 
 _LOG = LogService.get_logger("assetlibrary.assetsettingspanel")
 
 _LOC = os.path.dirname(__file__)
 ASSET_SETTINGS_PROPERTIES_DIR = os.path.join(_LOC, "properties")
 ASSET_SETTINGS_PROPERTIES = SceneConfigSet.from_definitions_in_json_directory(ASSET_SETTINGS_PROPERTIES_DIR, prefix="ASLS_")
+
+_CURRENT_SECOND_ROOT = None
 
 class MPFB_PT_Asset_Settings_Panel(Abstract_Panel):
     """Settings for loading asset files."""
@@ -53,6 +57,11 @@ class MPFB_PT_Asset_Settings_Panel(Abstract_Panel):
             "procedural_eyes"
             ])
 
+    def _second_root(self, scene, layout):
+        box = layout.box()
+        box.label(text="Asset roots")
+        ASSET_SETTINGS_PROPERTIES.draw_properties(scene, box, ["second_root"])
+
     def _assets(self, scene, layout):
         box = layout.box()
         box.label(text="Install assets")
@@ -67,7 +76,18 @@ class MPFB_PT_Asset_Settings_Panel(Abstract_Panel):
         self._draw_eyes(scene, layout)
         self._draw_mhclo(scene, layout)
         self._assets(scene, layout)
+        self._second_root(scene, layout)
 
+        global _CURRENT_SECOND_ROOT
+
+        if _CURRENT_SECOND_ROOT is None:
+            _CURRENT_SECOND_ROOT = LocationService.get_second_root()
+
+        sr = ASSET_SETTINGS_PROPERTIES.get_value("second_root", entity_reference=bpy.context.scene)
+        if sr != _CURRENT_SECOND_ROOT:
+            _LOG.debug("Second root changed", sr)
+            AssetService.update_all_asset_lists()
+            _CURRENT_SECOND_ROOT = sr
 
 ClassManager.add_class(MPFB_PT_Asset_Settings_Panel)
 
