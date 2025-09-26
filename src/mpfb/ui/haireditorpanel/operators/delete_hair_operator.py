97687829edf7b9d85bb2419ce85243952ebb4ce4
TomasKlecer <klecer@hotmail.com>
2025-09-26 15:46:00 +0200

    hair/fur asset delete button added
    - Delete hair operator cleaned from dead code and added to haireditorpanel
    
    - Added check in the haireditorpanel if the hair asset in properties actually exists. If not, UI drawer for this asset will not be drawn

--- src/mpfb/ui/haireditorpanel/operators/delete_hair_operator.py
+++ src/mpfb/ui/haireditorpanel/operators/delete_hair_operator.py
@@ -26,11 +26,11 @@ class MPFB_OT_DeleteHair_Operator(bpy.types.Operator):
         scene = context.scene
 
         self.report({'INFO'}, ("Deleting hair asset..."))
-
+    
         # Get Human object
         human_obj = context.object
-        if not human_obj or human_obj.name != 'Human':
-            self.report({'ERROR'}, "Object 'Human' must be active")
+        if not human_obj:
+            self.report({'ERROR'}, "Human object must be active")
             return {'CANCELLED'}
 
         # Get hair and eventual card assets
@@ -67,41 +67,6 @@ class MPFB_OT_DeleteHair_Operator(bpy.types.Operator):
             return {'CANCELLED'}
         if card_obj:
             bpy.data.objects.remove(card_obj, do_unlink=True)
-
-        # Force _cards_generated and _cards_baked to be false
-        prop_name = f"{self.hair_asset}_cards_generated"
-        if hasattr(scene, prop_name):
-            setattr(scene, prop_name, False)
-        prop_name = f"{self.hair_asset}_cards_baked"
-        if hasattr(scene, prop_name):
-            setattr(scene, prop_name, False)
-
-
-        # Remove assetes properties
-        prop_prefix = f"{self.hair_asset}_"
-        for name in [
-            "length", "density", "thickness", "frizz", "roll", "roll_radius",
-            "roll_length", "clump", "clump_distance", "clump_shape", "clump_tip_spread",
-            "noise", "noise_distance", "noise_scale", "noise_shape",
-            "curl", "curl_guide_distance", "curl_radius", "curl_frequency",
-            "holes", "holes_scale",
-            "fur_asset_open", "hair_asset_open",
-            "_cards_scale", "_cards_density", "_cards_placement",
-            "_cards_resolution", "_cards_samples", "_cards_glossy", "_cards_generated", "_cards_texture_dst", "_cards_baked"
-        ]:
-            prop_id = f"{prop_prefix}{name}"
-            if hasattr(scene.__class__, prop_id):
-                delattr(scene.__class__, prop_id)
-
-        # Remove material properties
-        mat_name = f"{self.hair_asset}"
-        material_props = [
-            "color1", "color2", "coror_noise_scale", "darken_root", "root_color_length"
-        ]
-        for name in material_props:
-            prop_id = f"{mat_name}_{name}"
-            if hasattr(scene.__class__, prop_id):
-                delattr(scene.__class__, prop_id)
 
         # Delete material
         material = bpy.data.materials.get(self.hair_asset)
