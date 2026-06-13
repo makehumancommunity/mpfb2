b96b0c8a0aafe1a5ba5c17c0d5ec0962864ec56a
Joel Palmius <joepal1976@hotmail.com>
2026-06-13 10:21:04 +0200

    Remove superfluous colon from labels

--- src/mpfb/services/humanservice.py
+++ src/mpfb/services/humanservice.py
@@ -1695,7 +1695,7 @@ class HumanService:
             except ValueError as exc:
                 if "generated rigify" in str(exc).lower():
                     message = ("Cannot refit: the rig is a generated rigify rig and no meta rig is "
-                               "present in the scene. Set 'Meta-rig:' to Keep or Hide on the rigify "
+                               "present in the scene. Set 'Meta-rig' to Keep or Hide on the rigify "
                                "sub-panel before generating to allow refitting on the next generate.")
                     _LOG.warn(message)
                     if operator is not None:
